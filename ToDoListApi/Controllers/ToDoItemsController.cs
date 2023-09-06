b5b1115dd6ea794fc4c2cdb5352c804a7b3f4249
emrah <emrah@GURDIAN>
2023-09-06 12:40:09 +0300

    Add project files.

---
+++ ToDoListApi/Controllers/ToDoItemsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoListApi.Data;
+
+//https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-7.0&tabs=visual-studio
+
+namespace ToDoListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ToDoItemsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public List<ToDoItem> GetToDoItems()
+        {
+            return _db.ToDoItems.OrderBy(t => t.Done).ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ToDoItem> GetToDoItem(int id)
+        {
+            var toDoItem = _db.ToDoItems.Find(id);
+
+            if (toDoItem == null)
+                return NotFound();
+
+            return toDoItem;
+        }
+
+        [HttpPost]
+        public ActionResult<ToDoItem> PostToDoItem(ToDoItem toDoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(toDoItem);
+                _db.SaveChanges();
+                return CreatedAtAction(nameof(GetToDoItem), new { id = toDoItem.Id }, toDoItem);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteToDoItem(int id)
+        {
+            var toDoItem = _db.ToDoItems.Find(id);
+
+            if (toDoItem == null)
+                return NotFound();
+
+            _db.Remove(toDoItem);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutToDoItem(int id, ToDoItem toDoItem)
+        {
+            if (id != toDoItem.Id)
+                return BadRequest();
+
+            if (!_db.ToDoItems.Any(t => t.Id == id))
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(toDoItem);
+                _db.SaveChanges();
+
+                return NoContent();
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ ToDoListApi/Program.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.EntityFrameworkCore;
+using ToDoListApi.Data;
+
+namespace ToDoListApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDbContext<ApplicationDbContext>(ob => ob.UseSqlServer(builder.Configuration.GetConnectionString("DbCon")));
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
